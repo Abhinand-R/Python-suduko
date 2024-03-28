10bec3f3ef8e0efbe1ca3185eab7a50303578df6
Abhinand-R <abhinand62003@gmail.com>
2024-03-28 23:28:01 +0530

    backtrcking2

--- solver.py
+++ solver.py
@@ -16,10 +16,18 @@ def solve(bo):
     if not find:
         return True
     else:
-        rol,col=find
+        row,col=find
 
+    for i in range(1,10):
+        if valid(bo,i,(row,col)):
+            bo[row][col]=i
 
+            if solve(bo):
+                return True
+            
+            bo[row][col]=0
 
+    return False
 
 
 
